fee768b8366c19c8b63de546c8baa4071a78d57e
Abanteeka <acharyaabanteeka196@gmail.com>
2022-11-14 23:03:38 +0530

    update

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -8,7 +8,7 @@ usg2 = "python NetworkScanner.py <TARGET> <PORT>"  # scan_2
 usg3 = "python NetworkScanner.py <TARGET>"  # scan_3
 
 
-def scanPortINRange():  # scanning in range,given by the user
+def ScanPortInRange():  # scanning in range,given by the user
     start_time = time.time()
 
     if len(sys.argv) != 4:
@@ -18,7 +18,7 @@ def scanPortINRange():  # scanning in range,given by the user
     try:
         target = socket.gethostbyname(sys.argv[1])
     except socket.gaierror:
-        print("Error")
+        print("Error Occurred")
         sys.exit()
 
     start_port = int(sys.argv[2])
@@ -45,5 +45,85 @@ def scanPortINRange():  # scanning in range,given by the user
 
     sys.exit()
 
-def ScanAllPorts():
-    print("hi")
+
+def ScanAllPorts():  # all ports are scanned
+    start_time = time.time()
+
+    if len(sys.argv) != 2:
+        print(usg3)
+        sys.exit()
+
+    try:
+        target = socket.gethostbyname(sys.argv[1])
+    except socket.gaierror:
+        print("Error Occurred")
+        sys.exit()
+
+    start_port = 1
+    end_port = 65535
+
+    print("Target: {}".format(target))
+
+    print("scanning process is started...")
+
+    def scan_port_2(port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1.5)
+        con = s.connect_ex((target, port))
+        if not con:
+            print("[#] Port is open {}".format(port))
+        s.close()
+
+    for Port in range(start_port, end_port + 1):
+        t = threading.Thread(target=scan_port_2, args=(Port,))
+        t.start()
+    end_time = time.time()
+    print("Time Taken: ", end_time - start_time)
+
+    sys.exit()
+
+
+def ScanOnePort():
+    start_time = time.time()
+
+    if len(sys.argv) != 3:
+        print(usg2)
+        sys.exit()
+
+    try:
+        target = socket.gethostbyname(sys.argv[1])
+    except socket.gaierror:
+        print("Error Occurred")
+        sys.exit()
+
+    print("Target: {}".format(target))
+
+    print("scanning process is started...")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2.5)
+    con = s.connect_ex((target, int(sys.argv[2])))
+    if not con:
+        print("[#] Port is open {}".format(sys.argv[2]))
+    else:
+        print("[#] Port is closed".format(sys.argv[2]))
+    s.close()
+
+    end_time = time.time()
+    print("Time Taken: ", end_time - start_time)
+
+    sys.exit()
+
+
+while True:
+    if len(sys.argv) == 4:
+        ScanPortInRange()
+    elif len(sys.argv) == 2:
+        ScanAllPorts()
+    elif len(sys.argv) == 3:
+        ScanOnePort()
+    else:
+        print("Format: " + usg1)
+        print("Format: " + usg2)
+        print("Format: " + usg3)
+        break
